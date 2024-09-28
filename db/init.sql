bc5c53f388c9b3c6371533e63e04012719d4ab57
GachiLord <name504172@gmail.com>
2024-09-28 16:38:33 +0500

    add database schema

---
+++ db/init.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    login varchar(36) NOT NULL,
+    name varchar(120) NOT NULL,
+    password varchar(97) NOT NULL,
+);
+
+CREATE IF NOT EXISTS UNIQUE INDEX id_idx ON users(id);
+
+
+CREATE TABLE IF NOT EXISTS tasks (
+  id SERIAL PRIMARY KEY,
+  complexity smallint NOT NULL,
+  CHECK (complexity >= 0 AND complexity <=3),
+  expected_time real NOT NULL,
+  CHECK (expected_time > 0),
+  tags text[] NOT NULL,
+  created_at timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE UNIQUE INDEX id_idx ON tasks(id);
+
+
+CREATE TABLE IF NOT EXISTS completed_tasks (
+  id SERIAL PRIMARY KEY,
+  CONSTRAINT fk_users
+    FOREIGN KEY(user_id) 
+	    REFERENCES users(id)
+	    ON DELETE SET NULL,
+  CONSTRAINT fk_tasks
+    FOREIGN KEY(task_id) 
+	    REFERENCES tasks(id)
+	    ON DELETE CASCADE
+);
+
+CREATE IF NOT EXISTS UNIQUE INDEX task_id_idx ON completed_tasks(task_id);
+CREATE IF NOT EXISTS UNIQUE INDEX user_id_idx ON completed_tasks(user_id);
+
+
+CREATE TABLE IF NOT EXISTS expired_jwts (
+	  id SERIAL PRIMARY KEY,
+	  jwt_data VARCHAR (500) NOT NULL,
+    expire_date DATE NOT NULL DEFAULT CURRENT_DATE
+);
+
+CREATE INDEX IF NOT EXISTS jwt_data_idx ON expired_jwts USING HASH (jwt_data);
